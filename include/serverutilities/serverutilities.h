0f9d71aca8e9a7c1509ec758e073d3ab6d4b71da
Bernard-wuam <holiest_2001@yahoo.co.in>
2026-09-04 23:55:40 -0700

    add files to tracking

---
+++ include/serverutilities/serverutilities.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "servererror/servererror.h"
+#include <boost/beast.hpp>
+#include <boost/beast/core/error.hpp>
+#include <boost/beast/http/message_fwd.hpp>
+#include <boost/beast/http/string_body_fwd.hpp>
+#include <boost/json.hpp>
+#include <boost/json/fwd.hpp>
+#include <boost/json/parse.hpp>
+#include <boost/mysql/datetime.hpp>
+#include <expected>
+#include <optional>
+#include <system_error>
+
+namespace ServerUtilities {
+template <typename T>
+inline std::expected<T, std::error_code> convertBodyToObject(
+    boost::beast::http::request<boost::beast::http::string_body> &request) {
+  boost::beast::error_code ec;
+
+  auto obj = boost::json::parse(request.body(), ec);
+  if (ec)
+    return std::unexpected(
+        makeErrorCode(ServerError::Convert_Body_To_Object_Failed));
+  auto tryVal = boost::json::try_value_to<T>(obj);
+  if (tryVal.has_error()) {
+    return std::unexpected(
+        makeErrorCode(ServerError::Convert_Body_To_Object_Failed));
+  }
+  return tryVal.value();
+}
+
+template <typename T>
+inline std::optional<T> convertBodyToObject(const std::string &val,
+                                            std::error_code &ec) {
+
+  auto obj = boost::json::parse(val, ec);
+  if (ec)
+    return std::nullopt;
+  auto tryVal = boost::json::try_value_to<T>(obj);
+  if (tryVal.has_error())
+    return std::nullopt;
+  return tryVal.value();
+}
+
+inline std::string formatTime(const boost::mysql::datetime &datetime) {
+  auto time = datetime.as_time_point();
+  auto timeString = std::format("{:%Y-%m-%d %H:%M:%S}", time);
+
+  auto findDotPos = timeString.rfind(".");
+  if (findDotPos != timeString.npos) {
+    timeString = timeString.substr(0, findDotPos);
+  }
+  return timeString;
+}
+inline std::string getImageExtention(const std::string &target) {
+  auto extentionIndex = target.rfind(".");
+  auto extention = target.substr(extentionIndex + 1, target.size());
+  std::string imageExtention = "image/";
+  imageExtention.append(extention);
+  return imageExtention;
+}
+} // namespace ServerUtilities
